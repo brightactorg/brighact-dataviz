26d9b8c2af90874677d405fd84ee6b2f47de1fa4
Owner <vtsoneva@gmail.com>
2020-06-29 11:53:17 +0300

    mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,13 +13,14 @@ module.exports = {
         purple: '#7854F2',
       },
       gridTemplateColumns: {
-        'similar-fractions': '2fr 1.2fr 1fr',
+        'three-big-indent': '2.5fr 1.5fr 1fr',
         'two-thirds': '2fr 1fr',
         'tho-thirds-inverse': '1fr 2fr',
         'three-to-one': '3fr 1fr',
         'similar': '1.2fr 1fr',
         'similar-reverse': '1fr 1.2fr',
-        'three-indent-half': '0.5fr 2fr 2fr 2fr'
+        'three-indent-half': '0.5fr 2fr 2fr 2fr',
+        'one-and-five': "1fr 5fr"
       },
       gridTemplateRows: {
         'two-thirds-inverse': '1fr 2fr',
